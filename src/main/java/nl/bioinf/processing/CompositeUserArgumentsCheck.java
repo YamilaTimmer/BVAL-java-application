2f3279805783e2bf404e53785845ebf1d079521a
YamilaTimmer <y.timmer.2@st.hanze.nl>
2025-10-11 20:57:59 +0200

    Implemented CompositeUserArgumentsCheck to now run all filter steps in succession. Using lambdas runnables for the actual filter steps itself are added to a list and ran in succession after all input arguments are validated

--- src/main/java/nl/bioinf/processing/CompositeUserArgumentsCheck.java
+++ src/main/java/nl/bioinf/processing/CompositeUserArgumentsCheck.java
@@ -1,27 +1,22 @@
 package nl.bioinf.processing;
 
-
-import nl.bioinf.model.MethylationArray;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class CompositeUserArgumentsCheck implements UserArgumentsCheck {
-    private List<UserArgumentsCheck> filters = new ArrayList<>();
+public class CompositeUserArgumentsCheck implements UserArgumentsCheck{
+    private final List<UserArgumentsCheck> filters = new ArrayList<>();
 
     public void addFilter(UserArgumentsCheck filter) {
         filters.add(filter);
     }
 
     // Goes through all checks for all arguments
-    @Override
-    public boolean pass(MethylationArray methylationArray) {
+    public boolean pass() {
         for (UserArgumentsCheck filter : filters) {
-            if (! filter.pass(methylationArray)) {
+            if (! filter.pass()) {
                 return false;
             }
         }
         return true;
     }
-
 }
