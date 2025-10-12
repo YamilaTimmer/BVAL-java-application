1206e7d7b08970e4c0f2e8653991d307f1f7c6d7
YamilaTimmer <y.timmer.2@st.hanze.nl>
2025-10-12 18:38:12 +0200

    Added javadoc to SummaryGenerator + added debug/info logging

--- src/main/java/nl/bioinf/processing/SummaryGenerator.java
+++ src/main/java/nl/bioinf/processing/SummaryGenerator.java
@@ -2,23 +2,34 @@ package nl.bioinf.processing;
 
 import nl.bioinf.model.MethylationArray;
 import nl.bioinf.model.MethylationData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Generates short summary for input file
+ */
 public class SummaryGenerator {
+    private static final Logger logger = LogManager.getLogger(SummaryGenerator.class.getName());
 
+    /**
+     * @param methylationData parsed data from input file, including samples, chromosomes/genes and betavalues
+     */
     public static void generateSummary(MethylationArray methylationData){
-        System.out.println("Generating summary...");
-        System.out.println("---------------------");
+        logger.info("Generating summary...");
 
-        // Get methylationdata/sampledata
+        // Get methylation data/sample data
         List<String> samples = methylationData.getSamples();
         List<MethylationData> dataRows = methylationData.getData();
-        int amountNAValues = 0;
 
+        int amountNAValues = 0;
         double betaVal = 0.0;
 
+        // Add value of all beta values to calculate the average
+        logger.debug("Calculating average beta values...");
         for (MethylationData row : dataRows) {
             ArrayList<Double> betas = row.betaValues();
             for (double beta : betas) {
@@ -26,18 +37,21 @@ public class SummaryGenerator {
                     betaVal += beta;
                     continue;
                 }
+                // If betaVal is not >1, add it to the amount of NA values (MethylationFileReader parses NA to -1)
                 amountNAValues++;
-
             }
         }
 
-        betaVal = betaVal / (dataRows.size() * samples.size());
+        double avgBetaVal = betaVal / (dataRows.size() * samples.size());
 
         // Print general info on file
+        System.out.println("Summary for input file:");
         System.out.println("Number of samples: " + samples.size());
         System.out.println("Number of genes: " + dataRows.size());
-        System.out.println("Avg beta value: " + Math.round(betaVal* 100.0) / 100.0); //2 decimals
+        System.out.println("Avg beta value: " + Math.round(avgBetaVal* 100.0) / 100.0); //2 decimals
         System.out.println("Amount of NA values: " + amountNAValues);
-    }
 
+        logger.debug("Summary generated successfully.");
+
+    }
 }
